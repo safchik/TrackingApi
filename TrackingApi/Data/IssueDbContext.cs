04c4c6860ea86ba2911122e54cd935a047b2adfd
Dmytro <sagchik9@gmial.com>
2023-12-18 12:42:17 +0000

    database context setup

---
+++ TrackingApi/Data/IssueDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TrackingApi.Models;
+
+namespace TrackingApi.Data
+{
+    public class IssueDbContext: DbContext
+    {
+        public IssueDbContext(DbContextOptions<IssueDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Issue> Issues { get; set; }
+    }
+}
